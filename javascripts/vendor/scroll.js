a7ca7b4c06f6146c318a78ebdfec53956ff24bb6
Peter Lai <peter.yd.lai@gmail.com>
2013-03-31 16:33:56 -0400

    updated spazzing scrolling

--- javascripts/vendor/scroll.js
+++ javascripts/vendor/scroll.js
@@ -2,31 +2,38 @@ $(function(){
 
   var scrolltime = 500;
 
-  $('#welcome-nav').click(function(){
+  $('#welcome-nav').click(function(ev){
+    ev.preventDefault();
     $.scrollTo('#welcome', scrolltime );
   });
 
-  $('#ourstory-nav').click(function(){
+  $('#ourstory-nav').click(function(ev){
+    ev.preventDefault();
     $.scrollTo('#ourstory', scrolltime );
   });
 
-  $('#weddingevents-nav').click(function(){
+  $('#weddingevents-nav').click(function(ev){
+    ev.preventDefault();
     $.scrollTo('#weddingevents', scrolltime );
   });
 
-  $('#location-nav').click(function(){
+  $('#location-nav').click(function(ev){
+    ev.preventDefault();
     $.scrollTo('#location', scrolltime );
   });
 
-  $('#registry-nav').click(function(){
+  $('#registry-nav').click(function(ev){
+    ev.preventDefault();
     $.scrollTo('#registry', scrolltime );
   });
 
-  $('#photos-nav').click(function(){
+  $('#photos-nav').click(function(ev){
+    ev.preventDefault();
     $.scrollTo('#photos', scrolltime );
   });
 
-  $('#rsvp-nav').click(function(){
+  $('#rsvp-nav').click(function(ev){
+    ev.preventDefault();
     $.scrollTo('#rsvp', scrolltime );
   });
 
